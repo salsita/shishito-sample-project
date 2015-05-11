e0002ea14fc6178397a49ac30b9dedef2d531c34
Filip Bures <filipb@salsitasoft.com>
2015-05-11 16:38:08 +0200

    Refactor example test runner.

--- google_test_runner.py
+++ google_test_runner.py
@@ -2,13 +2,14 @@ import os
 
 from shishito.salsa_runner import ShishitoRunner
 
-# from shishito.library.support.jira_zephyr_api import ZAPI
-
 
 class GoogleRunner(ShishitoRunner):
     """ Dedicated project runner, extends general SalsaRunner """
+
     def __init__(self):
-        self.project_root = os.path.dirname(os.path.abspath(__file__))
-        ShishitoRunner.__init__(self)
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        super(GoogleRunner, self).__init__(project_root)
+
 
 runner = GoogleRunner()
+runner.run_tests()
