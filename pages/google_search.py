40ef2862dd3a212fcb3db9883af2c421c85257a9
Filip Bures <filipb@salsitasoft.com>
2015-05-12 13:17:13 +0200

    Modify google search page according to CR

--- pages/google_search.py
+++ pages/google_search.py
@@ -14,23 +14,18 @@ class GoogleSearch(Page):
         Page.__init__(self, driver)
         self.driver = driver
 
-        self._search_field_locator = (By.ID, 'lst-ib')
-        self._luck_locator = (By.NAME, 'btnI')
-        self._jagr_title = (By.CLASS_NAME, 'kno-ecr-pt')
-        self._search_button = (By.NAME, 'btnG')
-
     @property
     def search_field(self):
-        return self.driver.find_element(*self._search_field_locator)
+        return self.driver.find_element(By.ID, 'lst-ib')
 
     @property
     def luck(self):
-        return self.driver.find_element(*self._luck_locator)
+        return self.driver.find_element(By.NAME, 'btnI')
 
     @property
     def jagr_title(self):
-        return self.driver.find_element(*self._jagr_title)
+        return self.driver.find_element(By.CLASS_NAME, 'kno-ecr-pt')
 
     @property
     def search_button(self):
-        return self.driver.find_element(*self._search_button)
+        return self.driver.find_element(By.NAME, 'btnG')
