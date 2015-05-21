ffbd47cb0c1de53d09acc7907f4f5bb455208978
Filip Bures <filipb@salsitasoft.com>
2015-05-21 13:26:03 +0200

    Clean config files
      * Remove unused config files
      * Add config file for generic tests
      * Update example .sh scripts

--- shishito_examples.sh
+++ shishito_examples.sh
@@ -21,18 +21,22 @@ export PYTHONPATH=${PYTHONPATH}:/${shishito_framework}
 #      TESTS
 # -----------------
 
+# DEKSTOP COMBINATIONS - non selenium tests
+python ${shishito_test_project}/google_test_runner.py --platform generic --environment local --test_directory non_selenium_tests
+python ${shishito_test_project}/google_test_runner.py --platform generic --environment local --test_directory non_selenium_tests --smoke
+
 # DESKTOP COMBINATIONS - local browser
-python ${shishito_test_project}/google_test_runner.py --platform web --environment local
-python ${shishito_test_project}/google_test_runner.py --platform web --environment local --smoke
+python ${shishito_test_project}/google_test_runner.py --platform web --environment local --test_directory tests
+python ${shishito_test_project}/google_test_runner.py --platform web --environment local --test_directory tests --smoke
 
 # DESKTOP COMBINATIONS - browserstack
-python ${shishito_test_project}/google_test_runner.py --platform web --environment browserstack --browserstack ${browserstack_user}:${browserstack_pass}
-python ${shishito_test_project}/google_test_runner.py --platform web --environment browserstack --browserstack ${browserstack_user}:${browserstack_pass} --smoke
+python ${shishito_test_project}/google_test_runner.py --platform web --environment browserstack --browserstack ${browserstack_user}:${browserstack_pass} --test_directory tests
+python ${shishito_test_project}/google_test_runner.py --platform web --environment browserstack --browserstack ${browserstack_user}:${browserstack_pass} --test_directory tests --smoke
 
 # MOBILE APPS - local appium server
-python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium
-python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --smoke
+python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --test_directory appium_tests
+python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --test_directory appium_tests --smoke
 
 # MOBILE APPS - using saucelabs
-python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --saucelabs ${saucelabs_user}:${saucelabs_pass}
-python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --saucelabs ${saucelabs_user}:${saucelabs_pass} --smoke
+python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --saucelabs ${saucelabs_user}:${saucelabs_pass} --test_directory appium_tests
+python ${shishito_test_project}/google_test_runner.py --platform mobile --environment appium --saucelabs ${saucelabs_user}:${saucelabs_pass} --test_directory appium_tests --smoke
